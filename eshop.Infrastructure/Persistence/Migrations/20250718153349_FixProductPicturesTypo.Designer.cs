fc2c57266ee5920fe2ff912ff58628505fecb7d8
matic113 <faressafi203@gmail.com>
2025-07-18 18:35:10 +0300

    fix: added migration for fixing product picture typo

--- eshop.Infrastructure/Persistence/Migrations/20250718153349_FixProductPicturesTypo.Designer.cs
+++ eshop.Infrastructure/Persistence/Migrations/20250718153349_FixProductPicturesTypo.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using eshop.Infrastructure.Persistence;
@@ -11,9 +12,11 @@ using eshop.Infrastructure.Persistence;
 namespace eshop.Infrastructure.Persistence.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250718153349_FixProductPicturesTypo")]
+    partial class FixProductPicturesTypo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ eshop.Infrastructure/Persistence/Migrations/20250718153349_FixProductPicturesTypo.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eshop.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixProductPicturesTypo : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "product_pictures");
+
+            migrationBuilder.CreateTable(
+                name: "product_picture",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    picture_url = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_product_picture", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_product_picture_product_product_id",
+                        column: x => x.product_id,
+                        principalTable: "product",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_product_picture_product_id",
+                table: "product_picture",
+                column: "product_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "product_picture");
+
+            migrationBuilder.CreateTable(
+                name: "product_pictures",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    product_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    picture_url = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_product_pictures", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_product_pictures_product_product_id",
+                        column: x => x.product_id,
+                        principalTable: "product",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_product_pictures_product_id",
+                table: "product_pictures",
+                column: "product_id");
+        }
+    }
+}
