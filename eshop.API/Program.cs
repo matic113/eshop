e434e8362e7ef799c783af56295f064689445953
matic113 <faressafi203@gmail.com>
2025-07-19 12:29:11 +0300

    feat: auto-migrate db to lastest on app startup

--- eshop.API/Program.cs
+++ eshop.API/Program.cs
@@ -19,6 +19,8 @@ if (app.Environment.IsDevelopment())
     app.MapScalarApiReference();
 }
 
+await app.ConfigurePersistence();
+
 app.UseHttpsRedirection();
 
 app.UseAuthentication();
---
+++ eshop.Infrastructure/ConfigureApp.cs
@@ -0,0 +1,21 @@
+﻿using eshop.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace eshop.Infrastructure
+{
+    public static class ConfigureApp
+    {
+        public static async Task ConfigurePersistence(this WebApplication app)
+        {
+            await app.EnsureDatabaseCreated();
+        }
+        private static async Task EnsureDatabaseCreated(this WebApplication app)
+        {
+            using var scope = app.Services.CreateScope();
+            var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await db.Database.MigrateAsync();
+        }
+    }
+}
