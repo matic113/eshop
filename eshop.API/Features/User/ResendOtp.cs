b6604168fd9366e03201fb8fcf6184aa1f4f09c3
matic113 <faressafi203@gmail.com>
2025-07-20 03:22:50 +0300

    feat: added an endpoint to resend the email verification otp

---
+++ eshop.API/Features/User/ResendOtp.cs
@@ -0,0 +1,58 @@
+﻿using eshop.Application.Contracts;
+using eshop.Infrastructure.Persistence;
+using FastEndpoints;
+using FluentValidation;
+using Microsoft.AspNetCore.Identity;
+
+namespace eshop.API.Features.User
+{
+    public class ResendOtp
+    {
+        sealed class ResendOtpRequest
+        {
+            public required string Email { get; set; }
+        }
+        sealed class ResendOtpValidator : Validator<ResendOtpRequest>
+        {
+            public ResendOtpValidator()
+            {
+                RuleFor(x => x.Email)
+                    .NotEmpty().WithMessage("Email address is required.")
+                    .EmailAddress().WithMessage("Invalid email address format.");
+            }
+        }
+
+        sealed class ResendOtpEndpoint : Endpoint<ResendOtpRequest>
+        {
+            private readonly UserManager<ApplicationUser> _userManager;
+            private readonly IOtpService _otpService;
+
+            public ResendOtpEndpoint(UserManager<ApplicationUser> userManager, IOtpService otpService)
+            {
+                _userManager = userManager;
+                _otpService = otpService;
+            }
+
+            public override void Configure()
+            {
+                Post("/api/auth/resend-otp");
+                AllowAnonymous();
+            }
+
+            public override async Task HandleAsync(ResendOtpRequest r, CancellationToken c)
+            {
+                var user = await _userManager.FindByEmailAsync(r.Email);
+
+                if (user is not null)
+                {
+                    var otp = await _otpService.GenerateOtpAsync(user.Id);
+                    await _otpService.SendOtpEmailAsync(otp, user.Email!);
+                }
+
+                // Regardless of whether the user exists or not, we do not want to reveal if the email is registered.
+
+                await SendOkAsync("OTP has been resent to your email.", c);
+            }
+        }
+    }
+}
