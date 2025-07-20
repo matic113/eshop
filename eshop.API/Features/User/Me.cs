ca166790a549409aa770192e118a254b1666e052
matic113 <faressafi203@gmail.com>
2025-07-20 10:11:41 +0300

    feat: added endpoint for getting current user info

---
+++ eshop.API/Features/User/Me.cs
@@ -0,0 +1,59 @@
+﻿using eshop.Infrastructure.Extensions;
+using eshop.Infrastructure.Persistence;
+using FastEndpoints;
+using Microsoft.AspNetCore.Identity;
+
+namespace eshop.API.Features.User
+{
+    public class Me
+    {
+        sealed class CurrentUserInfoResponse
+        {
+            public Guid UserId { get; set; }
+            public string Email { get; set; }
+            public string FullName { get; set; }
+        }
+        sealed class CurrentUserInfoEndpoint : EndpointWithoutRequest<CurrentUserInfoResponse>
+        {
+            private readonly UserManager<ApplicationUser> _userManager;
+
+            public CurrentUserInfoEndpoint(UserManager<ApplicationUser> userManager)
+            {
+                _userManager = userManager;
+            }
+
+            public override void Configure()
+            {
+                Get("/api/auth/me");
+            }
+
+            public override async Task HandleAsync(CancellationToken c)
+            {
+                var currentUserId = User.GetUserId();
+
+                if (currentUserId is null)
+                {
+                    await SendUnauthorizedAsync();
+                    return;
+                }
+
+                var user = await _userManager.FindByIdAsync(currentUserId.Value.ToString());
+
+                if (user is null)
+                {
+                    await SendNotFoundAsync(c);
+                    return;
+                }
+
+                var response = new CurrentUserInfoResponse
+                {
+                    UserId = user.Id,
+                    Email = user.Email ?? string.Empty,
+                    FullName = user.ToString() ?? string.Empty
+                };
+
+                await SendOkAsync(response, c);
+            }
+        }
+    }
+}
---
+++ eshop.Infrastructure/Extensions/ClaimsPrincipalExtensions.cs
@@ -0,0 +1,19 @@
+﻿using System.Security.Claims;
+
+namespace eshop.Infrastructure.Extensions
+{
+    public static class ClaimsPrincipalExtensions
+    {
+        public static Guid? GetUserId(this ClaimsPrincipal user)
+        {
+            var userIdString = user.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            if (string.IsNullOrEmpty(userIdString) || !Guid.TryParse(userIdString, out var userId))
+            {
+                return null;
+            }
+
+            return userId;
+        }
+    }
+}
