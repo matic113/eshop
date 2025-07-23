37a1bdc89c237a4ac2e520176cf172cec2d3c395
matic113 <faressafi203@gmail.com>
2025-07-23 19:06:21 +0300

    fix: update Paymob Options to add public key

--- eshop.Infrastructure/Payment/PaymobOptions.cs
+++ eshop.Infrastructure/Payment/PaymobOptions.cs
@@ -5,6 +5,7 @@
         public const string PaymobOptionsKey = "PaymobOptions";
         public required string ApiKey { get; set; }
         public required string SecretKey { get; set; }
+        public required string PublicKey { get; set; }
         public required int CardIntegrationId { get; set; }
     }
 }
