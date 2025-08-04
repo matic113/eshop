8d8e43796625021f0062be5b6136ecc95c04cc98
matic113 <faressafi203@gmail.com>
2025-08-04 14:17:33 +0300

    feat: added logout endpoint

---
+++ eshop.API/Features/User/Logout.cs
@@ -0,0 +1,38 @@
+﻿using FastEndpoints;
+
+namespace eshop.API.Features.User
+{
+    public class Logout
+    {
+        sealed class LogoutEndpoint : EndpointWithoutRequest
+        {
+            public override void Configure()
+            {
+                Post("/api/auth/logout");
+                Description(x => x
+                    .WithTags("Auth")
+                    .WithDescription("Sets access_token and refresh_token cookies to empty.")
+                    );
+            }
+
+            public override async Task HandleAsync(CancellationToken c)
+            {
+                var httpRespone = HttpContext.Response;
+
+                var cookieOptions = new CookieOptions
+                {
+                    HttpOnly = true,
+                    Secure = true,
+                    SameSite = SameSiteMode.Strict,
+                    Expires = DateTime.UtcNow.AddDays(-1)
+                };
+
+                // Clear access token cookie
+                httpRespone.Cookies.Append("access_token", string.Empty, cookieOptions);
+                httpRespone.Cookies.Append("refresh_token", string.Empty, cookieOptions);
+
+                await SendOkAsync();
+            }
+        }
+    }
+}
