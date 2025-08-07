20127e30060bd35aa28973f5b0bb977a6b0ed9fa
matic113 <faressafi203@gmail.com>
2025-08-07 22:52:34 +0300

    feat: added coupons table

---
+++ eshop.Infrastructure/Persistence/Configurations/CouponsUsageConfiguration.cs
@@ -0,0 +1,28 @@
+﻿using eshop.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eshop.Infrastructure.Persistence.Configurations
+{
+    public class CouponsUsageConfiguration : IEntityTypeConfiguration<CouponsUsage>
+    {
+        public void Configure(EntityTypeBuilder<CouponsUsage> builder)
+        {
+            builder.HasKey(cu => cu.Id);
+            
+            builder.HasIndex(cu => new { cu.UserId, cu.CouponId })
+                .IsUnique();
+
+            // Relationships
+            builder.HasOne(cu => cu.Coupon)
+                .WithMany(c => c.CouponsUsages)
+                .HasForeignKey(cu => cu.CouponId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            builder.HasOne<ApplicationUser>()
+                .WithMany()
+                .HasForeignKey(cu => cu.UserId)
+                .OnDelete(DeleteBehavior.Cascade);
+        }
+    }
+}
