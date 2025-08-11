18c3de6b46f20972b878a8b2381f10adf551f0b9
matic113 <faressafi203@gmail.com>
2025-08-11 16:24:04 +0300

    feat: added a static class for application roles

---
+++ eshop.Infrastructure/Authentication/ApplicationRoles.cs
@@ -0,0 +1,9 @@
+﻿namespace eshop.Infrastructure.Authentication
+{
+    public static class ApplicationRoles
+    {
+        public const string Admin = "Admin";
+        public const string Seller = "Seller";
+        public const string Buyer = "Buyer";
+    }
+}
